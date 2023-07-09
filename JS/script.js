58647b408f3be9a602fbc99d30e065324b2c4a1e
Deivison Novossate <deivisonnra@gmail.com>
2023-07-09 19:08:17 -0300

    created scroll menu smoth

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,7 @@ window.addEventListener("scroll", () => {
     
 })
 
+/* Menu Fixed */
 const scrollLogoDown = window.addEventListener('scroll', () => {
     let logoDark = document.querySelector('.logo-dark')
     let logoLight = document.querySelector('.logo-light')
@@ -19,7 +20,42 @@ const scrollLogoDown = window.addEventListener('scroll', () => {
         logoLight.classList.remove('hide', window.scrollY > 2)
     
     }
- 
-    
 })
+
+
+/* Scroll Smoth Sections */
+
+/* get links a */
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]')
+
+/* forEach em cada elemento a */
+menuLinks.forEach(link => {
+    link.addEventListener('click', scrollToOnClick)
+})
+
+/* Get element href and position in pixel the sections */
+function getScrollTopByHref(element){
+    const id = element.getAttribute('href')
+    return document.querySelector(id).offsetTop
+}
+
+/* Retiramos a função padrão do evento, pegamos exatamente o alvo do nosso click e linkamos no menu com scroll smoth */
+function scrollToOnClick(event){
+    event.preventDefault()
+    const element = event.target
+    const toSection = getScrollTopByHref(event.target)
+
+
+    scrollToPosition(toSection)
+}
+
+/* function smoth scroll position of the sections */
+
+function scrollToPosition(toSection){
+    window.scroll({
+        top: toSection,
+        behavior: "smooth"
+    
+    })
+}
 
